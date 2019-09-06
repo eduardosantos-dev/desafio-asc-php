0dce3a7df75583dbaa6dd17e6a2296e03c380a08
diinoprp <diinoprp@gmail.com>
2019-09-06 00:48:01 -0300

    Updates

---
+++ precache-manifest.cb88313af930a999cc221ca5f18898c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2287b259cb39466b862060fac80fa425",
+    "url": "/desafio-asc-php/index.html"
+  },
+  {
+    "revision": "669534aecddbb5c86d40",
+    "url": "/desafio-asc-php/static/css/main.70db32ee.chunk.css"
+  },
+  {
+    "revision": "7d74480b4718c7e39434",
+    "url": "/desafio-asc-php/static/js/2.57445f37.chunk.js"
+  },
+  {
+    "revision": "669534aecddbb5c86d40",
+    "url": "/desafio-asc-php/static/js/main.26ed917f.chunk.js"
+  },
+  {
+    "revision": "ff3a83c350fe4d578c83",
+    "url": "/desafio-asc-php/static/js/runtime~main.95deafa5.js"
+  }
+]);
